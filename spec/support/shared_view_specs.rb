6314323196132080edc8bd4fdf4e480f63c8f994
Zach Brock <zach@squareup.com>
2010-11-06 13:30:02 -0700

    move view spec and improve shared behavior

--- spec/support/shared_view_specs.rb
+++ spec/support/shared_view_specs.rb
@@ -1,6 +1,7 @@
 shared_examples_for "strict xhtml" do
   it "according to nokogiri" do
-    xml = render
+    template = example.example_group.top_level_description
+    xml = render :template => template
     xml = (xml =~ /^\s*<[^a-zA-Z]/) ? xml : "<root>#{xml}</root>"
     Nokogiri::XML.parse(xml, nil, nil, Nokogiri::XML::ParseOptions::STRICT)
   end
