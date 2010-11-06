5f3af74a32ed3e8795a9e5856d2312c0117f0bb3
Zach Brock <zach@squareup.com>
2010-11-06 11:55:08 -0700

    Setting up rspec and unicorn

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Billcrush::Application.config.secret_token = '507769cc1c80e0abe9db9c87c3f8096b0a8712b227dd0da8e753d13a38b56b18eb2ba5ccba16105f2f0690652eecd877eff1aa78d0c9e06a6562a6548aeac2d6'
+Billcrush::Application.config.secret_token = '9d856121f1ed0c1b51dce4d9a54a00422e9ba1078caf04adabc9b6c4ef76aea56076cc128e04216f61acfff920f4c6509f98fa0ef3188c07e94b008b125a0e5b'
