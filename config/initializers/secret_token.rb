5fefef2c38fe0484ad60ed16bb0f0fd42399c6eb
Zach Brock <zach@squareup.com>
2010-11-06 11:35:27 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Billcrush::Application.config.secret_token = '507769cc1c80e0abe9db9c87c3f8096b0a8712b227dd0da8e753d13a38b56b18eb2ba5ccba16105f2f0690652eecd877eff1aa78d0c9e06a6562a6548aeac2d6'
