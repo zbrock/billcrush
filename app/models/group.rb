ad873a2bf238dcf0f81ad31e963e1195836c9fb3
Zach Brock <zach@squareup.com>
2010-11-07 13:07:40 -0800

    Model work for figuring out best way to settle debts across the group

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,8 +4,30 @@ class Group < ActiveRecord::Base
   has_many :members
   has_many :transactions
 
+
+  def best_way_to_settle
+    settlement = []
+    balances = members.map { |m| {:balance => m.balance, :member => m}}
+    while balances.size > 0
+      balances = sort_and_filter(balances)
+      break if balances.size == 0
+      most_in_debt = balances.first
+      most_positive = balances.last
+      amount = [most_in_debt[:balance].abs, most_positive[:balance].abs].min.to_i
+      settlement << {:payer => most_in_debt[:member], :payee => most_positive[:member], :amount => amount}
+      most_in_debt[:balance] += amount
+      most_positive[:balance] -= amount
+    end
+    settlement
+  end
+
   private
   def canonicalize_name
     self.canonicalized_name = name.parameterize
+  end
+
+  def sort_and_filter(balances)
+    result = balances.sort_by{|b| b[:balance]}
+    result.reject{|b| b[:balance] == 0}
   end
 end
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe Group do
-  it{ should validate_presence_of(:name) }
-  it{ should have_many(:members) }
-  it{ should have_many(:transactions) }
+  it { should validate_presence_of(:name) }
+  it { should have_many(:members) }
+  it { should have_many(:transactions) }
   describe "life cycle" do
     describe "canonicalized_name" do
       [
@@ -17,6 +17,56 @@ describe Group do
           group.save!
           group.canonicalized_name.should == name[1]
         end
+      end
+    end
+  end
+  describe "#best_way_to_settle" do
+
+    it "returns an empty array if there are no members" do
+      Factory(:group).best_way_to_settle.should == []
+    end
+    context "with members" do
+      before do
+        @group = Factory(:group)
+        @member_one = Factory(:member, :group => @group)
+        @member_two = Factory(:member, :group => @group)
+        @member_three = Factory(:member, :group => @group)
+        @member_four = Factory(:member, :group => @group)
+      end
+
+      it "returns an empty array when there is no debt to settle" do
+        @group.best_way_to_settle.should == []
+      end
+
+      it "returns an array of hashes showing the best way to settle" do
+        Factory(:debt, :debtor => @member_one, :creditor => @member_two, :amount_cents => 10_00)
+        Factory(:debt, :debtor => @member_three, :creditor => @member_two, :amount_cents => 10_00)
+        Factory(:debt, :debtor => @member_two, :creditor => @member_one, :amount_cents => 5_00)
+        Factory(:debt, :debtor => @member_three, :creditor => @member_one, :amount_cents => 1_00)
+        Factory(:debt, :debtor => @member_one, :creditor => @member_four, :amount_cents => 1_00)
+        @member_one.balance.should == -5_00
+        @member_two.balance.should == 15_00
+        @member_three.balance.should == -11_00
+        @member_four.balance.should == 1_00
+
+        @group.best_way_to_settle.should include({:payer => @member_three, :payee => @member_two, :amount => 11_00})
+        @group.best_way_to_settle.should include({:payer => @member_one, :payee => @member_two, :amount => 4_00})
+        @group.best_way_to_settle.should include({:payer => @member_one, :payee => @member_four, :amount => 1_00})
+        @group.best_way_to_settle.size.should == 3
+      end
+
+      it "doesn't include people who don't have debt" do
+        Factory(:debt, :debtor => @member_one, :creditor => @member_two, :amount_cents => 10_93)
+        @member_one.balance.should == -10_93
+        @member_two.balance.should == 10_93
+        @group.best_way_to_settle.should == [{:payer => @member_one, :payee => @member_two, :amount => 10_93}]
+      end
+
+      it "returns an empty array if all the debts cancel" do
+        Factory(:debt, :debtor => @member_one, :creditor => @member_two, :amount_cents => 10_00)
+        Factory(:debt, :debtor => @member_two, :creditor => @member_three, :amount_cents => 10_00)
+        Factory(:debt, :debtor => @member_three, :creditor => @member_one, :amount_cents => 10_00)
+        @group.best_way_to_settle.should == []
       end
     end
   end
