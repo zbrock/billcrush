651f067da56230b526c1f7a2812cbf6f0af5fae7
Zach Brock <zach@squareup.com>
2010-11-07 01:45:36 -0700

    add method that moves throught the commit phases for a transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,4 +2,11 @@ class Transaction < ActiveRecord::Base
   has_many :debts
   belongs_to :group
   validates :group, :presence => true
+  validates_numericality_of :amount_cents, :greater_than => 0
+
+  def validate_and_activate!
+    return false if debts.sum(:amount_cents) != amount_cents
+    update_attributes(:active => true)
+    debts.update_all({:active => true})
+  end
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,8 +1,35 @@
 require 'spec_helper'
 
 describe Transaction do
-  it{ should have_many(:debts)}
-  it{ should belong_to(:group)}
+  it { should have_many(:debts) }
+  it { should belong_to(:group) }
   it { should validate_presence_of(:group) }
+  it { should validate_numericality_of(:amount_cents) }
 
+  describe "#validate_and_activate!" do
+    it "is false if there are no debts" do
+      transaction = Factory(:transaction)
+      transaction.validate_and_activate!.should be_false
+      transaction.reload.should_not be_active
+    end
+
+    it "is false and does not activate if the associated debt amounts don't add up to the amount_cents" do
+      transaction = Factory(:transaction, :amount_cents => 2000)
+      Factory(:debt, :amount_cents => 1000, :transaction => transaction, :active => false)
+      transaction.debts.each{|debt| debt.should_not be_active}
+      transaction.validate_and_activate!.should be_false
+      transaction.reload.should_not be_active
+      transaction.debts.each{|debt| debt.should_not be_active}
+    end
+
+
+    it "is true and activates if the associated debt amounts do add up to the amount cents" do
+      transaction = Factory(:transaction, :amount_cents => 1000)
+      Factory(:debt, :amount_cents => 1000, :transaction => transaction, :active => false)
+      transaction.debts.each{|debt| debt.should_not be_active}      
+      transaction.validate_and_activate!.should be_true
+      transaction.reload.should be_active
+      transaction.debts.each{|debt| debt.should be_active}
+    end
+  end
 end
