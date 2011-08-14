5d5dae1c6f1143fb95d1c24d98846ef313267dd8
Zach Brock <zbrock@gmail.com>
2011-08-14 15:47:21 -0700

    Add custom 404 page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,21 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActionController::RoutingError, :with => :render_404
+  rescue_from ActionController::UnknownAction, :with => :render_404
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+
   protect_from_forgery
   
   def amount_cents(amount_decimal)
     (amount_decimal.to_f * 100).round
   end
 
+  def render_404
+    if /(jpe?g|png|gif)/i === request.path
+      render :text => "404 Not Found", :status => 404
+    else
+      render :template => "shared/404", :layout => 'application', :status => 404
+    end
+  end
   private
   def load_group
     if params[:name].present?
