b3429dc6aa601d3bde9672fbfc8bd85ef8519354
Zach Brock <zbrock@gmail.com>
2011-08-15 07:51:10 -0700

    Add Google Analytics

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# Constants
+GOOGLE_ANALYTICS_CODE = "UA-3252564-8"
+
 # Initialize the rails application
 Billcrush::Application.initialize!
