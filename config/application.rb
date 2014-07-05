b255f06af0fc1a5250d820c3a1fc866ea49cfa10
Zach Brock <zbrock@gmail.com>
2014-07-05 14:27:05 -0700

    fix heroku issue with asset compilation on 3.2

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,9 @@ module Billcrush
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
+    
+    # Fix Heroku issue with db connections
+    config.assets.initialize_on_precompile = false
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
